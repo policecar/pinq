b4c37b58659407e9c66855d67bff11fd61fe03b1
Priska Herger <priska@23bit.net>
2013-09-02 23:58:52 +0200

    Added uniform and Gauss random primitives.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject pinq "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Probabilistic programming in Clojure."
+  :url "https://github.com/policecar/pinq"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]])
--- src/pinq/core.clj
+++ src/pinq/core.clj
@@ -1,6 +1,30 @@
 (ns pinq.core)
 
+;; various discrete and continuous random variables
+;; to serve as data types
+
+(defn uniform
+  "A discrete uniform random variable."
+  [k]
+  (/ 1 k))
+
+(defn uniformc
+  "A continuous uniform random variable."
+  [a b]
+  (/ 1 (- b a)))
+
+(defn bernoulli
+  "A Bernoulli random variable."
+  [p]
+  (if (< (rand) p) 1 0))
+
 (defn flip
-	"A Bernoulli random variable."
-	[p]
-	(if (< (rand) p) 1 0))
+  "A Bernoulli random variable."
+  [k]
+  (bernoulli k))
+
+(defn gauss
+  "A Gaussian random variable."
+  [mu sigma]
+  (import '(java.util Random))
+  (-> (Random.) .nextGaussian (* sigma) (+ mu)))
